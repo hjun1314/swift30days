4d7c28bc07707820618ab7efa76bef7e11ea3750
Jerry <huangjun0529@126.com>
2020-08-27 16:44:24 +0800

    Day05

--- probject/Day04/Day04/ViewController.swift
+++ probject/Day04/Day04/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+let screenWidth = UIScreen.main.bounds.size.width
+let screenHeight = UIScreen.main.bounds.size.height
+let cellid = "cellid"
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath)
+        as! listCell
+        cell.cellModel = datas[indexPath.item]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(bgView)
+        view.addSubview(collectionView)
+        
     }
-
+    
+    lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = CGSize.init(width:cellWidth , height:cellHeight)
+        let collectionView = UICollectionView.init(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(listCell.self, forCellWithReuseIdentifier: cellid)
+        collectionView.contentInset = UIEdgeInsets.init(top: 50, left: 50, bottom: 50, right: 50)
+        return collectionView
+    }()
+    
+    lazy var bgView : UIImageView = {
+        let bgView = UIImageView.init(frame: view.bounds)
+        bgView.image = UIImage.init(named: "blue")
+        let blurEffect = UIBlurEffect.init(style: .light)
+        let blurEffectView = UIVisualEffectView.init(effect: blurEffect)
+        blurEffectView.frame = bgView.frame
+        bgView.addSubview(blurEffectView)
+        return bgView
+    }()
+    
+    lazy var datas : [listCellModel] = {
+        let datas = [ listCellModel(image:"hello", title: "Hello there, i miss u."),
+               listCellModel(image:"dudu", title: "🐳🐳🐳🐳🐳"),
+               listCellModel(image:"bodyline", title: "Training like this, #bodyline"),
+               listCellModel(image:"wave", title: "I'm hungry, indeed."),
+               listCellModel(image:"darkvarder", title: "Dark Varder, #emoji"),
+    ]
+        return datas
+    
+    }()
 
 }
 
--- probject/Day04/Day04/listCell.swift
+++ probject/Day04/Day04/listCell.swift
@@ -7,7 +7,61 @@
 //
 
 import UIKit
+let cellWidth = 300
+let cellHeight = 450
+
+struct listCellModel {
+    var image : String
+    var title : String
+}
+
 
 class listCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        if !self.isEqual(nil) {
+            contentView.addSubview(imageView)
+            contentView.addSubview(blurEffectView)
+            blurEffectView.contentView.addSubview(titleLab)
+            layer.cornerRadius = 5
+            layer.masksToBounds = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var cellModel : listCellModel! {
+        didSet{
+            imageView.image = UIImage.init(named: cellModel.image)
+            titleLab.text = cellModel.title
+        }
+    }
+    
+    
+    //MARK: - 懒加载
+    //图片
+    lazy var imageView : UIImageView = {
+        let imageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: cellWidth, height: cellHeight))
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    //毛玻璃
+    lazy var blurEffectView : UIVisualEffectView = {
+        let blurEffect = UIBlurEffect.init(style: .light)
+        let blurEffectView = UIVisualEffectView.init(effect: blurEffect)
+        blurEffectView.frame = CGRect(x: 0, y: cellHeight - 50, width: cellWidth, height: 50)
+        return blurEffectView
+    }()
+    //标题
+    lazy var titleLab : UILabel = {
+        let titleLab = UILabel.init(frame: CGRect(x: 10, y: 10, width: cellWidth - 20, height: 30))
+        titleLab.font = UIFont.systemFont(ofSize: 16)
+        titleLab.textColor = .white
+        return titleLab
+    }()
     
 }
